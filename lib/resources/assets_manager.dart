a6ee1827fd4d21c4beee4e36e3e7f503a2c12af1
Salah Saleh <salahsaleh1015@gmail.com>
2024-10-26 21:08:26 +0300

    assets manager

---
+++ lib/resources/assets_manager.dart
@@ -0,0 +1,15 @@
+
+
+const String imagePath = "assets/images";
+const String jsonPath = "assets/json";
+
+
+class ImageAssets {
+
+  static const String splashLogo = "$imagePath/splash_logo.png";
+  static const String onBoardingOneImage = "$imagePath/on_boarding_one.png";
+  static const String onBoardingTwoImage = "$imagePath/on_boarding_two.png";
+  
+
+
+}
