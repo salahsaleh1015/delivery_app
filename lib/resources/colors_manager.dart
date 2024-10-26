3d6f6ad30026a9ec0783d52d41f73d62cd43561d
Salah Saleh <salahsaleh1015@gmail.com>
2024-10-26 21:08:16 +0300

    color manager

---
+++ lib/resources/colors_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = const Color(0xfffd683d);
+  static Color lightPrimary = const Color(0xfff4c0b1);
+  static Color white = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff000000);
+  static Color arrowBackBackGroundColor = const Color(0xfff3d5cc);
+  static Color secondaryTextColor = const Color(0xff666666);
+  static Color hintColor = const Color(0xffa6a4a8);
+  static Color inputFillColor = const Color(0xffe6e0e9);
+  static Color error = const Color(0xffe61f34);
+}
